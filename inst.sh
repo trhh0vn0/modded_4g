fcd69ce5be9c2f5c9c110d1c161c18c07e184e12
Truong Hoang Huu <106631901+trhh0vn0@users.noreply.github.com>
2024-11-04 00:19:50 +0700

    Add files via upload

---
+++ inst.sh
@@ -0,0 +1,14 @@
+#!/system/bin/sh
+
+clear
+echo "Đang cài đặt"
+apt update && apt upgrade -y
+clear
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ curl -O raw.githubusercontent.com/trhh0vn0/modded_4g/main/v111.zip && unzip v111.zip && chmod a+x neko && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x qexit && chmod a+x wl
+clear
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ curl -O https://raw.githubusercontent.com/trhh0vn0/modded_4g/main/t1 && mv t1 motd
+clear
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ curl -O https://raw.githubusercontent.com/trhh0vn0/modded_4g/main/l1 && chmod +x * && mv l1 login &&
+clear
+echo "Đã cài đặt thành công"
+login
